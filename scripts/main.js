8ca2652e57ac6317f00f70ed2cb19d7f6495bdd2
John Lynch <johnlynch.32768@gmail.com>
2023-03-31 17:59:35 +0100

    Make clock drift to top left and only then do intro wobble

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
+const id = id => document.getElementById(id);
+
+function set_time() {
+    const now = new Date();
+    const hour_hand = id('hour-hand');
+    const minute_hand = id('minute-hand');
+    const second_hand = id('second-hand');
+
+    let secs = now.getSeconds();
+    let mins = now.getMinutes();
+    let hours = now.getHours();
+
+    const hourStr = `${hours < 10 ? `0` : ``}${hours}`;
+    const minStr = `${mins < 10 ? `0` : ``}${mins}`;
+    const secStr = `${secs < 10 ? `0` : ``}${secs}`;
+
+    second_hand.style.transform.animationDelay = ``;
+    minute_hand.style.transform.animationDelay = ``;
+    hour_hand.style.transform.animationDelay = `-3600s`;
+}
+
 window.addEventListener("DOMContentLoaded", _ => {
     const clock = document.querySelector('.clock');
     const numerals = [...document.querySelectorAll('.clock-numeral')];
@@ -10,4 +31,6 @@ window.addEventListener("DOMContentLoaded", _ => {
         n.style.left = `${r * (1 + Math.cos(phi))}px`;
         n.style.top = `${r * (1 + Math.sin(phi))}px`;
     });
+    set_time();
 });
+
