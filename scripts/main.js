16e071331f44f6cc44a4e6c72fc883023b20b3de
John Lynch <johnlynch.32768@gmail.com>
2023-04-05 00:07:15 +0100

    Tweak styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,47 @@
 const id = id => document.getElementById(id);
 
-function set_time() {
-    const now = new Date();
-    const hour_hand = id('hour-hand');
-    const minute_hand = id('minute-hand');
-    const second_hand = id('second-hand');
-
-    let secs = now.getSeconds();
-    let mins = now.getMinutes();
-    let hours = now.getHours();
-
-    const hourStr = `${hours < 10 ? `0` : ``}${hours}`;
-    const minStr = `${mins < 10 ? `0` : ``}${mins}`;
-    const secStr = `${secs < 10 ? `0` : ``}${secs}`;
-
-    second_hand.style.transform.animationDelay = ``;
-    minute_hand.style.transform.animationDelay = ``;
-    hour_hand.style.transform.animationDelay = `-3600s`;
-}
-
 window.addEventListener("DOMContentLoaded", _ => {
     const clock = document.querySelector('.clock');
     const numerals = [...document.querySelectorAll('.clock-numeral')];
-    const cw = window.getComputedStyle(clock).width;
-    const nw = window.getComputedStyle(numerals[0]).width;
-    const r = +cw.slice(0, 3) / 2.0 - +nw.slice(0, 3);
-    numerals.forEach( n => {
-        const phi = n.dataset.angle * Math.PI / 180.0;
-        console.log(`r: ${r}, phi: ${phi}`);
-        n.style.left = `${r * (1 + Math.cos(phi))}px`;
-        n.style.top = `${r * (1 + Math.sin(phi))}px`;
-    });
+    position_numerals();
     set_time();
+    var cw, nw, r;
+
+    function position_numerals() {
+        cw = window.getComputedStyle(clock).width;
+        nw = window.getComputedStyle(numerals[0]).width;
+        r = +cw.slice(0, 3) / 2.0 - +nw.slice(0, 3);
+        numerals.forEach( n => {
+            const phi = n.dataset.angle * Math.PI / 180.0;
+            console.log(`r: ${r}, phi: ${phi}`);
+            n.style.left = `${r * (1 + Math.cos(phi))}px`;
+            n.style.top = `${r * (1 + Math.sin(phi))}px`;
+        });
+    }
+
+    function set_time() {
+        const now = new Date();
+        const hour_hand = id('hour-hand');
+        const minute_hand = id('minute-hand');
+        const second_hand = id('second-hand');
+
+        let secs = now.getSeconds();
+        let mins = now.getMinutes();
+        let hours = now.getHours();
+
+        // const hourStr = `${hours < 10 ? `0` : ``}${hours}`;
+        // const minStr = `${mins < 10 ? `0` : ``}${mins}`;
+        // const secStr = `${secs < 10 ? `0` : ``}${secs}`;
+
+        // second_hand.style.transform.animationDelay = ``;
+        // minute_hand.style.transform.animationDelay = ``;
+        // hour_hand.style.transform.animationDelay = `-3600s`;
+        // debugger;
+        // hour_hand.style.setProperty('--hour', '-1200s');
+
+    }
+
+window.addEventListener("resize", position_numerals);    
 });
 
            
