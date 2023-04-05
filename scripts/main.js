fb34df31e287203900a30eaf74bfee96aed09248
John Lynch <johnlynch.32768@gmail.com>
2023-04-05 03:11:14 +0100

    Add resposive shim to finely position numerals according to window size

--- scripts/main.js
+++ scripts/main.js
@@ -10,31 +10,27 @@ window.addEventListener("DOMContentLoaded", _ => {
     function position_numerals() {
         cw = window.getComputedStyle(clock).width;
         nw = window.getComputedStyle(numerals[0]).width;
-        r = +cw.slice(0, 3) / 2.0 - +nw.slice(0, 3);
+        r = +cw.slice(0, -2) / 2.0 - +nw.slice(0, -2);
         numerals.forEach( n => {
             const phi = n.dataset.angle * Math.PI / 180.0;
+            const shim = +window.getComputedStyle(clock.parentElement).width.slice(0, -2) / 420;
             // console.log(`r: ${r}, phi: ${phi}`);
-            n.style.left = `${r * (1 + Math.cos(phi))}px`;
+            n.style.left = `${r * (1 + Math.cos(phi)) + shim}px`;
             n.style.top = `${r * (1 + Math.sin(phi))}px`;
         });
     }
 
     function set_time() {
-        const hour_hand = id('hour-hand');
-        const minute_hand = id('minute-hand');
-        const second_hand = id('second-hand');
-        
         const now = new Date();
         const sec_delay = -now.getSeconds();
         const min_delay = -now.getMinutes() * 60 + sec_delay;
-        const hour_delay = -now.getHours() * 3600 + min_delay;
+        const hour_delay = -(now.getHours() % 12) * 3600 + min_delay;
         
         console.log([sec_delay, min_delay, hour_delay]);
-        // debugger;
 
-        second_hand.style.setProperty('--delay', `${sec_delay}s`);
-        minute_hand.style.setProperty('--delay', `${min_delay}s`);
-        hour_hand.style.setProperty('--delay', `${hour_delay}s`);
+        id('second-hand').style.setProperty('--delay', `${sec_delay}s`);
+        id('minute-hand').style.setProperty('--delay', `${min_delay}s`);
+        id('hour-hand').style.setProperty('--delay', `${hour_delay}s`);
     }
 
     window.addEventListener("resize", position_numerals);    
