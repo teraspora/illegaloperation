c3c5e44293f59429fc8ce84fcc4e6b0541dbddc7
John Lynch <johnlynch.32768@gmail.com>
2023-04-05 03:35:54 +0100

    Tidy code

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+/*
+
+Illegal Operation - A website by John Lynch - April 2023
+Main JS file
+*/
+
 const id = id => document.getElementById(id);
 
 window.addEventListener("DOMContentLoaded", _ => {
@@ -14,7 +20,6 @@ window.addEventListener("DOMContentLoaded", _ => {
         numerals.forEach( n => {
             const phi = n.dataset.angle * Math.PI / 180.0;
             const shim = +window.getComputedStyle(clock.parentElement).width.slice(0, -2) / 420;
-            // console.log(`r: ${r}, phi: ${phi}`);
             n.style.left = `${r * (1 + Math.cos(phi)) + shim}px`;
             n.style.top = `${r * (1 + Math.sin(phi))}px`;
         });
@@ -25,9 +30,6 @@ window.addEventListener("DOMContentLoaded", _ => {
         const sec_delay = -now.getSeconds();
         const min_delay = -now.getMinutes() * 60 + sec_delay;
         const hour_delay = -(now.getHours() % 12) * 3600 + min_delay;
-        
-        console.log([sec_delay, min_delay, hour_delay]);
-
         id('second-hand').style.setProperty('--delay', `${sec_delay}s`);
         id('minute-hand').style.setProperty('--delay', `${min_delay}s`);
         id('hour-hand').style.setProperty('--delay', `${hour_delay}s`);
