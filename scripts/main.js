912c80327c5de349d78eef355cf4cc22481d5ac1
John Lynch <johnlynch.32768@gmail.com>
2023-04-05 02:47:09 +0100

    Get clock telling the correct time

--- scripts/main.js
+++ scripts/main.js
@@ -13,35 +13,31 @@ window.addEventListener("DOMContentLoaded", _ => {
         r = +cw.slice(0, 3) / 2.0 - +nw.slice(0, 3);
         numerals.forEach( n => {
             const phi = n.dataset.angle * Math.PI / 180.0;
-            console.log(`r: ${r}, phi: ${phi}`);
+            // console.log(`r: ${r}, phi: ${phi}`);
             n.style.left = `${r * (1 + Math.cos(phi))}px`;
             n.style.top = `${r * (1 + Math.sin(phi))}px`;
         });
     }
 
     function set_time() {
-        const now = new Date();
         const hour_hand = id('hour-hand');
         const minute_hand = id('minute-hand');
         const second_hand = id('second-hand');
-
-        let secs = now.getSeconds();
-        let mins = now.getMinutes();
-        let hours = now.getHours();
-
-        // const hourStr = `${hours < 10 ? `0` : ``}${hours}`;
-        // const minStr = `${mins < 10 ? `0` : ``}${mins}`;
-        // const secStr = `${secs < 10 ? `0` : ``}${secs}`;
-
-        // second_hand.style.transform.animationDelay = ``;
-        // minute_hand.style.transform.animationDelay = ``;
-        // hour_hand.style.transform.animationDelay = `-3600s`;
+        
+        const now = new Date();
+        const sec_delay = -now.getSeconds();
+        const min_delay = -now.getMinutes() * 60 + sec_delay;
+        const hour_delay = -now.getHours() * 3600 + min_delay;
+        
+        console.log([sec_delay, min_delay, hour_delay]);
         // debugger;
-        // hour_hand.style.setProperty('--hour', '-1200s');
 
+        second_hand.style.setProperty('--delay', `${sec_delay}s`);
+        minute_hand.style.setProperty('--delay', `${min_delay}s`);
+        hour_hand.style.setProperty('--delay', `${hour_delay}s`);
     }
 
-window.addEventListener("resize", position_numerals);    
+    window.addEventListener("resize", position_numerals);    
 });
 
            
